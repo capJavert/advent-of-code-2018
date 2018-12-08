3dbb4a82b4540d991c7461cbb93ce91175bc0be8
capJavert <ante.baric3@gmail.com>
2018-12-08 10:17:30 +0100

    8-day part 2 completed

--- 8-day.js
+++ 8-day.js
@@ -7,14 +7,11 @@ async function main() {
     const tree = []
     let header = []
     let parents = []
-    let isMetaData = 0
-    let metaSum = 0
 
     input.forEach(n => {
         const current = tree[parents[parents.length - 1]]
         if (current) {
             if (!current.header[0] && current.header[1]) {
-                metaSum += +n
                 current.meta.push(+n)
                 current.header[1] -= 1
 
@@ -33,7 +30,7 @@ async function main() {
 
             if (current && current.header[0]) {
                 current.header[0] -= 1
-                current.children.push(id)
+                current.children.push(tree[id])
             }
             parents.push(id)
             header = []
@@ -44,7 +41,28 @@ async function main() {
         }
     })
 
-    console.log(metaSum)
+    const rootValue = tree[0].meta.reduce((value, meta) => {
+        if (tree[0].children.length) {
+            value += tree[0].children[meta - 1] ? nodeValue(tree[0].children[meta - 1]) : 0
+        } else {
+            value += meta
+        }
+
+        return value
+    }, 0)
+
+    console.log(rootValue)
+}
+
+function nodeValue(node) {
+    return node.meta.reduce((value, meta) => {
+        if (node.children.length) {
+            value += node.children[meta - 1] ? nodeValue(node.children[meta - 1]) : 0
+        } else {
+            value += meta
+        }
+        return value
+    }, 0)
 }
 
 main()
