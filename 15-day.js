f2486125c99e834584ebf9dfb442300820331b6b
capJavert <ante.baric3@gmail.com>
2018-12-17 00:31:49 +0100

    15-day part 2 completed

--- 15-day.js
+++ 15-day.js
@@ -2,6 +2,10 @@ const { getInput } = require('./utils')
 const aStar = require('a-star')
 const Graph = require('node-dijkstra')
 
+Object.prototype.toString = function() {
+    return `${this.y},${this.x}`
+}
+
 function compareByXy(keyA, keyB) {
     const a = {
         x: +keyA.split(',')[1],
@@ -77,7 +81,7 @@ async function main() {
     let input = data.split(/\r?\n/)
 
     let index = 1
-    const battlefield = input.reduce((acc, line, y) => {
+    const originalBattlefield = JSON.stringify(input.reduce((acc, line, y) => {
         line.trim().split('').forEach((pos, x) => {
             if (pos !== ' ') {
                 const field = {
@@ -96,134 +100,142 @@ async function main() {
             }
         })
         return acc
-    }, {})
+    }, {}))
 
-    console.log('init')
-    currentState(battlefield, input)
+    let elfWasted = true
+    let elfPower = 3
 
-    let isEnd = false
-    let rounds = 0
-    while(!isEnd) {
-        for (let keyA of Object.keys(battlefield).sort(compareByXy)) {
-            let unitA = battlefield[keyA]
+    while (elfWasted) {
+        const battlefield = JSON.parse(originalBattlefield)
+        elfPower += 1
+        elfWasted = false
+        let isEnd = false
+        let rounds = 0
+        while(!isEnd) {
+            for (let keyA of Object.keys(battlefield).sort(compareByXy)) {
+                let unitA = battlefield[keyA]
 
-            if (unitA.pos === '.' || unitA.pos === '#' || unitA.round > rounds) {
-                continue
-            }
-
-            let closest = { d: Infinity, unit: null }
-            let inRange = false
-            let hasOpponents = false
-
-            for (let neighbor of neighbors(unitA, battlefield)) {
-                if (neighbor.pos === (unitA.pos === 'G' ? 'E' : 'G')) {
-                    inRange = true
-                    hasOpponents = true
-                    break
+                if (unitA.pos === '.' || unitA.pos === '#' || unitA.round > rounds) {
+                    continue
                 }
-            }
 
-            let closestPath = null
+                let closest = { d: Infinity, unit: null }
+                let inRange = false
+                let hasOpponents = false
 
-            if (!inRange) {
-                const movePositions = []
-
-                const graph = mapToGraph(unitA, battlefield)
-
-                for (let keyB of Object.keys(battlefield).sort(compareByXy)) {
-                    const unitB = battlefield[keyB]
-
-                    if (unitB.pos === unitA.pos || unitB.pos === '.' || unitB.pos === '#') {
-                        continue
+                for (let neighbor of neighbors(unitA, battlefield)) {
+                    if (neighbor.pos === (unitA.pos === 'G' ? 'E' : 'G')) {
+                        inRange = true
+                        hasOpponents = true
+                        break
                     }
+                }
 
-                    hasOpponents = true
+                let closestPath = null
 
-                    for (let neighbor of neighbors(unitB, battlefield)) {
-                        if (neighbor.pos === '.') {
-                            movePositions.push(neighbor.toString())
+                if (!inRange) {
+                    const movePositions = []
+
+                    const graph = mapToGraph(unitA, battlefield)
+
+                    for (let keyB of Object.keys(battlefield).sort(compareByXy)) {
+                        const unitB = battlefield[keyB]
+
+                        if (unitB.pos === unitA.pos || unitB.pos === '.' || unitB.pos === '#') {
+                            continue
+                        }
+
+                        hasOpponents = true
+
+                        for (let neighbor of neighbors(unitB, battlefield)) {
+                            if (neighbor.pos === '.') {
+                                movePositions.push(neighbor.toString())
+                            }
                         }
                     }
-                }
 
-                for (let neighborKey of movePositions.sort(compareByXy)) {
-                    const neighbor = battlefield[neighborKey]
-                    const path = graph.path(unitA.toString(), neighbor.toString())
+                    for (let neighborKey of movePositions.sort(compareByXy)) {
+                        const neighbor = battlefield[neighborKey]
+                        const path = graph.path(unitA.toString(), neighbor.toString())
 
-                    if (path !== null) {
-                        path.shift()
-                        if (!closestPath || closestPath.length > path.length) {
-                            closestPath = path
-                        } else if (closestPath.length === path.length) {
-                            if (compareByXy(path[0], closestPath[0]) === -1) {
+                        if (path !== null) {
+                            path.shift()
+                            if (!closestPath || closestPath.length > path.length) {
                                 closestPath = path
+                            } else if (closestPath.length === path.length) {
+                                if (compareByXy(path[0], closestPath[0]) === -1) {
+                                    closestPath = path
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            if (!hasOpponents) {
-                isEnd = true
-                break
-            }
-
-            if (closestPath) {
-                const closestPathElem = battlefield[closestPath[0]]
-                const move = {
-                    ...unitA,
-                    index: closestPathElem.index,
-                    x: closestPathElem.x,
-                    y: closestPathElem.y
+                if (!hasOpponents) {
+                    isEnd = true
+                    break
                 }
-                battlefield[unitA.toString()] = {
-                    ...closestPathElem,
-                    index: unitA.index,
-                    x: unitA.x,
-                    y: unitA.y
-                }
-                battlefield[move.toString()] = move
-                unitA = battlefield[move.toString()]
-            }
 
-            let selectedTarget = null
-            for (let target of neighbors(unitA, battlefield)) {
-                if (target.pos === (unitA.pos === 'G' ? 'E' : 'G')) {
-                    if (!selectedTarget || target.hp < selectedTarget.hp) {
-                        selectedTarget = target
+                if (closestPath) {
+                    const closestPathElem = battlefield[closestPath[0]]
+                    const move = {
+                        ...unitA,
+                        index: closestPathElem.index,
+                        x: closestPathElem.x,
+                        y: closestPathElem.y
+                    }
+                    battlefield[unitA.toString()] = {
+                        ...closestPathElem,
+                        index: unitA.index,
+                        x: unitA.x,
+                        y: unitA.y
+                    }
+                    battlefield[move.toString()] = move
+                    unitA = battlefield[move.toString()]
+                }
+
+                let selectedTarget = null
+                for (let target of neighbors(unitA, battlefield)) {
+                    if (target.pos === (unitA.pos === 'G' ? 'E' : 'G')) {
+                        if (!selectedTarget || target.hp < selectedTarget.hp) {
+                            selectedTarget = target
+                        }
                     }
                 }
-            }
 
-            if (selectedTarget) {
-                selectedTarget.hp -= 3
+                if (selectedTarget) {
+                    selectedTarget.hp -= unitA.pos === 'E' ? elfPower : 3
 
-                if (selectedTarget.hp <= 0) {
-                    delete selectedTarget.hp
-                    selectedTarget.pos = '.'
+                    if (selectedTarget.hp <= 0) {
+                        if (selectedTarget.pos === 'E') {
+                            isEnd = true
+                            elfWasted = true
+                            break
+                        }
+
+                        delete selectedTarget.hp
+                        selectedTarget.pos = '.'
+                    }
                 }
+
+                unitA.round += 1
             }
 
-            unitA.round += 1
+            if (!isEnd) {
+                rounds += 1
+            }
         }
 
-        if (!isEnd) {
-            rounds += 1
-        }
+        const hpSum = Object.keys(battlefield).reduce((acc, key) => {
+            const unit = battlefield[key]
+            if (unit.hp) {
+                acc += unit.hp
+            }
+            return acc
+        }, 0)
 
-        console.log(rounds)
-        currentState(battlefield, input)
+        console.log(elfPower, rounds * hpSum)
     }
-
-    const hpSum = Object.keys(battlefield).reduce((acc, key) => {
-        const unit = battlefield[key]
-        if (unit.hp) {
-            acc += unit.hp
-        }
-        return acc
-    }, 0)
-
-    console.log(rounds * hpSum)
 }
 
 main()
