0167f174664ed8b111a13e889a1c2e7006f85461
Ante Barić <ante.baric@burza.hr>
2018-12-19 11:43:43 +0100

    WIP4

--- 17-day.js
+++ 17-day.js
@@ -10,13 +10,16 @@ function currentState(map, boundsI, boundsJ, c, silent) {
         }
 
         for (let j = boundsJ[0] - 1; j <= boundsJ[1] + 1; j += 1) {
-            // console.log(j)
+            if (!map[i][j]) {
+                map[i][j] = '.'
+            }
+
             if (i === 0 && j === 500) {
                 line += '+'
             } else if (i === c[0] && j === c[1]) {
-                line += 'X'
+                line += '|'
             } else {
-                line += map[i][j] ? map[i][j] : '.'
+                line += map[i][j]
             }
         }
 
@@ -31,7 +34,7 @@ let boundsY = [Infinity, 0]
 let veins = null
 
 async function main() {
-    //const data = await getInput('https://pastebin.com/raw/x76aZV0s')
+    // const data = await getInput('https://pastebin.com/raw/x76aZV0s')
     const data = await getInput('https://pastebin.com/raw/rdbr3Rp2')
     let input = data.split(/\r?\n/)
 
@@ -87,106 +90,83 @@ async function main() {
     }, {})
 
     currentState(veins, boundsY, boundsX, [], true)
-    // console.log(veins)
-
-    const visited = {}
-    let c = [0, 500]
-    let hasCapacity = true
-    /* while (hasCapacity) {
-        hasCapacity = false
-
-        let canMove = true
-        while (canMove) {
-            currentState(veins, boundsY, boundsX, [...c])
-            pause()
-            canMove = false
-
-
-            if (down(c, veins) === '#') {
-                c = down(c, veins, true)
-                canMove = true
-                hasCapacity = true
-                continue
-            } else if (down(c, veins) === '~') {
-
-            }
-            if (['#', '~'].indexOf(left(c, veins)) === -1) {
-                c = left(c, veins, true)
-                canMove = true
-                hasCapacity = true
-                continue
-            } else if (['#', '~'].indexOf(right(c, veins)) === -1) {
-                c = right(c, veins, true)
-                canMove = true
-                hasCapacity = true
-                continue
-            }
-        }
-    } */
-    while (true) {
-        flow(c, 'down')
-    }
-
-    let water = 0
-    Object.keys(veins).forEach(i => {
-        Object.keys(veins[i]).forEach(j => {
-            if (veins[i][j] === '~') {
-                water += 1
-            }
-        })
-    })
-
-    currentState(veins, boundsY, boundsX, c)
-    console.log(water)
 }
 
-function flow(c, d = 'down') {
-    currentState(veins, boundsY, boundsX, c)
-    pause()
+function fillFrom(c) {
 
-    if (c[0] <= boundsY[1]) {
-        if (['.', undefined].indexOf(down(c)) > -1) {
-            flow(down(c, true), 'down')
-        } else {
-            if (d === 'down') {
-                veins[c[0]][c[1]] = '~'
-                return
-            }
-
-            if (['.', undefined].indexOf(left(c)) > -1) {
-                flow(left(c, true), 'left')
-            } else if (['.', undefined].indexOf(down(left(c, true))) > -1) {
-                flow(down(left(c, true), true), 'dleft')
-            } else {
-                if (d === 'left') {
-                    veins[c[0]][c[1]] = '~'
-                    return
-                }
-            }
-            if (['.', undefined].indexOf(right(c)) > -1) {
-                flow(right(c, true), 'right')
-            } else if (['.', undefined].indexOf(down(right(c, true))) > -1) {
-                flow(down(right(c, true), true), 'dright')
-            } else {
-                if (d === 'right') {
-                    veins[c[0]][c[1]] = '~'
-                    return
-                }
-            }
-        }
-    }
 }
 
-function down(c, move) {
-    return !move ? veins[c[0] + 1][c[1]] : [c[0] + 1, c[1]]
+function fillLevel(c) {
+
 }
 
-function left(c, move) {
-    return !move ? veins[c[0]][c[1] - 1] : [c[0], c[1] - 1]
+function hasBothWalls(c) {
+
 }
 
-function right(c, move) {
-    return !move ? veins[c[0]][c[1] + 1] : [c[0], c[1] + 1]
+main().then(() => {
+    const maxY = boundsY[1]
+    const minY = boundsY[0]
+    const maxX = boundsX[1]
+    const minX = boundsX[0]
+    fillFrom(veins, [500, 0], fillFrom);
+    currentState(veins, boundsY, boundsX, [0, 500])
+})
+
+// fillFrom(veins, [500, 0], fillFrom);
+// printGrid(grid, '', '');
+
+/* const flowing = pipe(grid.slice(minY, maxY + 1))(
+  x => flatten<string>(x),
+  sumBy(s => (s === '|' ? 1 : 0)),
+);
+const resting = pipe(grid.slice(minY, maxY + 1))(
+  x => flatten<string>(x),
+  sumBy(s => (s === '~' ? 1 : 0)),
+);
+console.log(flowing + resting);
+console.log(resting); */
+
+function fillFrom(grid, [x, y], self) {
+    console.log(x, y)
+  if (y >= maxY) return;
+  if (grid[y + 1][x] === '.') {
+    grid[y + 1][x] = '|';
+    fillFrom(grid, [x, y + 1], self);
+  }
+  if ('#~'.includes(grid[y + 1][x]) && grid[y][x + 1] === '.') {
+    grid[y][x + 1] = '|';
+    fillFrom(grid, [x + 1, y], self);
+  }
+  if ('#~'.includes(grid[y + 1][x]) && grid[y][x - 1] === '.') {
+    grid[y][x - 1] = '|';
+    fillFrom(grid, [x - 1, y], self);
+  }
+  if (hasBothWalls(grid, [x, y])) fillLevel(grid, [x, y]);
 }
 
-main()
+function hasBothWalls(grid, pos) {
+  return hasWall(grid, pos, 1) && hasWall(grid, pos, -1);
+}
+function hasWall(grid, [x, y], xOffset = 1) {
+  let currentX = x;
+  while (true) {
+    if (grid[y][currentX] === '.') return false;
+    if (grid[y][currentX] === '#') return true;
+    currentX += xOffset;
+  }
+}
+
+function fillLevel(grid, pos) {
+  fillSide(grid, pos, 1), fillSide(grid, pos, -1);
+}
+function fillSide(grid, [x, y], xOffset = 1) {
+  let currentX = x;
+  while (true) {
+    if (grid[y][currentX] === '#') return;
+    grid[y][currentX] = '~';
+    currentX += xOffset;
+  }
+}
+
+// main()
